bd7b24398af097a95fc8dcd668c52e514c349315
Tntimothy <tntimothyjp28@gmail.com>
2025-11-23 00:47:59 +0800

    Updated app.py, added model
    hairmodel is in
    app.py updated
    removed conversion.py

--- app.py
+++ app.py
@@ -1,47 +1,54 @@
-import numpy as np
+import os
+from flask import Flask, request, jsonify
 import tensorflow as tf
+import numpy as np
 from tensorflow.keras.utils import load_img, img_to_array
-from flask import Flask, request
+
+from tooscarysoheresanewfile import recommend_products, generate_explanation
 
 app = Flask(__name__)
 
-MODEL_PATH = "model/hair_type_efficientnetb3_finetuned.h5"
+# Load CNN model
+MODEL_PATH = "hair_type_efficientnetb3_finetuned.h5"
 IMG_SIZE = (300, 300)
 CLASS_NAMES = ["Straight", "Wavy", "Curly"]
 
 model = tf.keras.models.load_model(MODEL_PATH, compile=False)
 
-def predict_hair_type(image_path):
-    img = load_img(image_path, target_size=IMG_SIZE)
+def predict_hair_type(img_path):
+    img = load_img(img_path, target_size=IMG_SIZE)
     img_array = img_to_array(img)
     img_array = tf.keras.applications.efficientnet.preprocess_input(img_array)
     img_array = np.expand_dims(img_array, axis=0)
 
     preds = [model.predict(img_array, verbose=0) for _ in range(10)]
     avg_pred = np.mean(preds, axis=0)
-
-    predicted_class = CLASS_NAMES[np.argmax(avg_pred)]
+    hair_type = CLASS_NAMES[np.argmax(avg_pred)]
     confidence = float(np.max(avg_pred) * 100)
-    return predicted_class, confidence
+    return hair_type, confidence
 
-@app.route('/analyze_image', methods=['POST'])
-def analyze_image():
-    if 'image' not in request.files:
-        return {"error": "No image uploaded"}, 400
+@app.route("/predict", methods=["POST"])
+def predict():
+    if "file" not in request.files:
+        return jsonify({"error": "No file uploaded"})
 
-    file = request.files['image']
-    filepath = "temp_image.jpg"
+    file = request.files["file"]
+    filepath = os.path.join("temp_image.jpg")
     file.save(filepath)
 
+    # CNN
     hair_type, confidence = predict_hair_type(filepath)
 
+    # NLP
     recommendations = recommend_products(hair_type)
+    explanation = generate_explanation(hair_type, confidence, recommendations)
 
-    final_output = generate_explanation(hair_type, confidence, recommendations)
-
-    return {
+    return jsonify({
         "hair_type": hair_type,
         "confidence": confidence,
         "recommendations": recommendations,
-        "explanation": final_output
-    }
+        "explanation": explanation
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
